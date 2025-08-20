2ee12a109a5e225272f7ae375a032c0607a3203c
Code_With_Creator <145642591+creator2104@users.noreply.github.com>
2025-08-20 21:09:21 +0530

    learnt about error handeling

--- src/app.js
+++ src/app.js
@@ -2,54 +2,24 @@ const express = require('express');
 
 const app = express()
 
-const {adminAuth , userAuth} = require("./middleware/auth")
+// err should be the first parameter in the middleware function
+app.use("/",(err, req, res, next)=>{
+  if(err){
+   res.status(501).send("something went wrong")
+  }
+})
 
-// use of middleware below 
-
-// we have two apis of admin getAllData and deleteUser and we have to write logic multipletimes so we will use middleware to avoid code repetition
-/*
-app.get("/admin/getAllData",(req,res)=>{
-// this req should be authenticated and only admin can access this route
-   const token = "xyz"
-   const isAdminauthorized = token === "xyz"  
-   if(isAdminauthorized){
-      res.send("All data fetched successfully")
-      // by default res.send will send 200 status code
+app.get("/getUserData",(req,res)=>{
+   try{
+      // Logic of DB call and get user data
+      throw new Error("Error in getting user data") 
+   res.send("User data sent")
    }
-   else{
-      res.status(403).send("Unauthorized request")
-      // here we are sending 403 status code which means forbidden
-   } 
-})
-
-app.get("/admin/deleteUser",(req,res)=>{
-const token = "xyz"
-const isAdminauthorized = token === "xyz"  
-   if(isAdminauthorized){
-      res.send("User deleted successfully")
+   catch(err){
+      res.status(500).send("Internal Server Error")
    }
-   else{
-      res.status(403).send("Unauthorized request")
-   } 
 })
-*/
-// we have to rewrite the same logic of authentication for both the apis so we will use middleware to avoid code repetition
-// generally we use app.use() in middleware
 
-
-// Handle auth middleware for all rquests GET, POST, PUT, DELETE
-app.use("/admin",adminAuth)
-
-// i have only one route for user so i will write userAuth inside the route
-app.get("/user", userAuth , (req,res)=>{
-      res.send("user data sent successfully")
-})
-app.get("/admin/getAllData",(req,res)=>{
-      res.send("All data fetched successfully")
-})
-app.get("/admin/deleteUser",(req,res)=>{
-      res.send("User deleted successfully")
-})
 app.listen(3000, () => {
 console.log("Server is running on port 3000");
 })
--- src/pr.md
+++ src/pr.md
@@ -253,3 +253,31 @@ console.log("Server is running on port 3000");
 })
 
 ------------------------------------------------------part 4 of 3---------------------------------------------------
+const express = require('express');
+
+const app = express()
+
+// err should be the first parameter in the middleware function
+app.use("/",(err, req, res, next)=>{
+  if(err){
+   res.status(501).send("something went wrong")
+  }
+})
+
+app.get("/getUserData",(req,res)=>{
+   try{
+      // Logic of DB call and get user data
+      throw new Error("Error in getting user data") 
+   res.send("User data sent")
+   }
+   catch(err){
+      res.status(500).send("Internal Server Error")
+   }
+})
+
+app.listen(3000, () => {
+console.log("Server is running on port 3000");
+})
+
+------------------------------------------------------part 1 of 4---------------------------------------------------
+
