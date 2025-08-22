781acf8760b4c737b71bc25b826810326499a39d
Code_With_Creator <145642591+creator2104@users.noreply.github.com>
2025-08-23 02:45:11 +0530

    sanitized some data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database"); // Import the database connectionn
 const app = express();
-const User = require("./models/user"); 
+const User = require("./models/user");
 
 // the data will come from postman in json format so we need to tell express to parse the json data into js object
 // and then we can access the data using req.body and using new instance of the User model and then we can save the data to the database using .save() method
@@ -9,73 +9,77 @@ const User = require("./models/user");
 app.use(express.json());
 
 // GET user by Email ID
-app.get("/user",async (req,res)=>{
-   try{
-    const users = await User.find({emailId: req.body.emailId})
-    if(!users){
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    if (!users) {
       res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
-    else{
-      res.send(users)
-    }
-   //  aisa user dhundho jiska emailId req.body.emailId ke barabar ho and then us user ko return kar do and also res.send karke postman ko bhej do
-   if(users.length===0){
+    //  aisa user dhundho jiska emailId req.body.emailId ke barabar ho and then us user ko return kar do and also res.send karke postman ko bhej do
+    if (users.length === 0) {
       return res.status(404).send("User not found");
-   }
-   else{
-      res.send(users)
-   }
-   }
-   catch(err){
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
     res.status(400).send("Error fetching email");
-   }
-})
+  }
+});
 
 // Feed API - GET/feed - get all users from the database
 app.get("/feed", async (req, res) => {
-try{
-   const users = await User.find({});
-   res.send(users);
-}
-catch(err){
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
     res.status(400).send("Error fetching email");
-}
+  }
 });
 
 // Delete a user from the database
-app.delete("/user", async (req, res)=>{
-   const userId = req.body.userId;
-   try{
-      // const deletedUser = await User.findByIdAndDelete({_id:userId});
-      const deletedUser = await User.findByIdAndDelete(userId); 
-      if(!deletedUser){
-         return res.status(404).send("User not found");
-      }
-      res.send("User deleted successfully");
-   }
-   catch(err){
-      res.status(400).send("Error deleting user");
-   }
-
-})
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // const deletedUser = await User.findByIdAndDelete({_id:userId});
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Error deleting user");
+  }
+});
 
 // UPDATE a user in the database
-app.patch("/user", async (req, res)=>{
-   const userId = req.body.userId;
-   const data = req.body
-   try{
-      await User.findByIdAndUpdate({_id: userId}, data,{
-         returnDocument: 'before',
-         runValidators: true
-         // runValidators is used when we are updating the user and we want to validate the data that we are updating so when we update the age while updating the user it will work otherwise it will work only while new document will created 
-      }) 
-      // the above line will take the userId and the data that we want to update and it will update the user in the database , the returnDocument: 'before' will return the document before the update means the object before the update
-      res.send("User updated successfully");
-}
-catch(err){
-      res.status(400).send("Error updating user");
-   }
-})
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if(data?.skills.length>10){
+      throw new Error("Skills cannot be more than 10")
+    }
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "before",
+      runValidators: true,
+      // runValidators is used when we are updating the user and we want to validate the data that we are updating so when we update the age while updating the user it will work otherwise it will work only while new document will created
+    });
+    // the above line will take the userId and the data that we want to update and it will update the user in the database , the returnDocument: 'before' will return the document before the update means the object before the update
+    res.send("User updated successfully");
+  } catch (err) {
+   console.log(err);
+    res.status(400).send("Error updating user");
+  }
+});
 // app.get("/user/:id",async (req,res)=>{
 //    try{
 //     const users = await User.findById(req.params.id)
@@ -92,13 +96,13 @@ catch(err){
 // })
 
 app.post("/signup", async (req, res) => {
-   // whenever we do database operations like read or write we should wrap it in a try catch block
+  // whenever we do database operations like read or write we should wrap it in a try catch block
   try {
-     // create a new instance of the User model
+    // create a new instance of the User model
     const user = new User(req.body);
-   //  once u creted a user instance then u can save it to the database
-     await user.save();
-   //  save method returns a promise
+    //  once u creted a user instance then u can save it to the database
+    await user.save();
+    //  save method returns a promise
     res.send("User added successfully");
   } catch (err) {
     console.error(err);
@@ -119,5 +123,4 @@ connectDB()
     console.error("DB connection failed", err);
   });
 
-
-// now we will pass data to the database dynamically using postman 
+// now we will pass data to the database dynamically using postman
